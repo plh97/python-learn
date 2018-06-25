0ded9fab3cc8160a60a79857d506fac058197c74
pengliheng <pengliheng111@gmail.com>
2018-06-26 07:42:58 +0800

    finish my follow other peoples script with nodejs

--- pu.js
+++ pu.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const dely = ms => new Promise(res=> setTimeout(res,ms));
 
 (async (ctx) => {
-  let pageIndex = 36;
+  let pageIndex = 37;
   const browser = await puppeteer.launch({
     // 想要代码运的好，还得首选金丝雀
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
