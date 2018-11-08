c054640dbb18946c21cae3006816b587a7817032
pengliheng <pengliheng111@gmail.com>
2018-11-08 22:33:07 +0800

    add api postcss

--- qt5/UTCTime.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtGui
-from PyQt5.QtCore import QDateTime, QDate, QTime,Qt
-
-datatime = QDateTime.currentDateTime()
-
-print("Local Date And Time Is " + datatime.toString(Qt.DefaultLocaleLongDate))
-print("Universal Date And Time Is " + datatime.toUTC().toString())
-
-print("The offset From UTC Is {0}: Seconds ".format(datatime.offset))
